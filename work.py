14f48030982871b58ad13102a6f15ca7d44a80a3
mwachala <berrybretch@gmail.com>
2020-08-20 18:28:25 +0300

    Doctstrings and annotations. Not much progress here.

--- work.py
+++ work.py
@@ -11,15 +11,19 @@ import sys
 ###
 sys.setrecursionlimit(50000)
 
+#grab threadmark, timestamp and thread content
+#check last thread with last saved thread( by comparing timestamps ) and then update or not
+
 class SpaceBattler:
     def __init__(self, link):
         self.link = link+'/reader' if 'reader' not in link else link
         self.ses = requests.Session()
 
-    def find_links(self):
-        #response test
-        #link test?
-        #return number of pages instead?
+    def find_links(self) -> int:
+        '''
+        Takes link{reader_mode} to spacebattles thread, and returns the number of 
+        pages/links in the story
+        '''
         response = self.ses.get(self.link)
         soup = BeautifulSoup(response.text, 'lxml')
         pages = soup.find_all('li', class_='pageNav-page')
@@ -29,7 +33,14 @@ class SpaceBattler:
             number_of_links = int(pages[-1].string)
             return number_of_links
 
-    def grab_content(self, page_number):
+    def grab_content(self, page_number:int) -> tuple:
+        '''
+        Takes single page number, combines with link and eats it.
+        Sends request through self.session
+        Goes through single page response and grabs all threads and threadmarkers
+        Combines both in dictionary
+        Returns tuple of content and page number because i need to keep track of it for some reason
+        '''
         response = self.ses.get(f'{self.link}/page-{page_number}')
         soup = BeautifulSoup(response.text, 'lxml')
         content = soup.find_all('div', class_='bbWrapper')
@@ -38,19 +49,17 @@ class SpaceBattler:
         page = {k:v for k in chapters for v in content}
         return (page_number, page)
 
-    def execution(self):
+    def execution(self) -> list:
+        '''
+        Creates threads for each page request because thats what cool kids do.
+        Grabs content asynchronously.
+        Returns a list of tuples from the grab_content func
+        '''
         number_of_links = self.find_links() + 1
         with ThreadPoolExecutor() as exec:
             futures = exec.map(self.grab_content, range(1, number_of_links))
         ans = list(futures)
         return ans
-
-
-
-        #pass all pages in
-        #yield pages joined to page number
-        #return all pages in order?
-        pass
 
 
     def epub_pack(self):
@@ -62,8 +71,5 @@ class SpaceBattler:
 if __name__ == "__main__":
     onePageTest = "https://forums.spacebattles.com/threads/bastille-worm-hamefura.873739"
     severalPageTest= "https://forums.spacebattles.com/threads/compulsion-worm-prototype.821228"
-    x =SpaceBattler("https://forums.spacebattles.com/threads/compulsion-worm-prototype.821228")
-    y = x.find_links()
-    z = x.execution()
-    pickle.dump(z, open('sus', 'wb'))
-    pprint(y)
+    x =SpaceBattler()
+    
