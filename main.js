4458c55118e8fbbcb2f8340ee3a0389528a5a5fd
mwachala <berrybretch@gmail.com>
2020-08-31 18:54:19 +0300

    Trying to promisify...??

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
+const cheerio = require("cheerio")
 const fetch = require("node-fetch")
+const link = "https://forums.spacebattles.com/threads/compulsion-worm-prototype.821228/reader/"
 
-const link = "https://forums.spacebattles.com/threads/compulsion-worm-prototype.821228/reader/page-"
-list_of_content = []
-for (var i=1;i<10;++i){
-    list_of_content.push(`${link}${i}`)
-}
 
-const all_of_them = (links) => {
-    const content = []
-    links.forEach(element => {
-        content.push(fetch(element))      
-    });
-    return Promise.all(content)
-}
 
-all_of_them(list_of_content)
-.then(content => {
-    debugger;
-    console.log(content)
+
+const page_futures = (story_link) =>{
+    futures = []
+    //get single return, parse for page numbers
+    fetch(story_link)
+        .then((response) => {
+            response.text()      
+        })
+        .then(data => {
+            //create container to store html
+            //parse info from container using cheerio
+            let docs = cheerio.load(data)
+            console.log(docs)
+        }) 
+        .catch(error => console.log(error))
+} 
+
+
+page_futures(link)
+
+const fireWalkWithMe = new Promise((resolve, reject) => {
+    //tbfleshedoutinamomentplswait
 })
-.catch(e => console.log(e))
+
+
+
+
+
 
